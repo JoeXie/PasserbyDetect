2c67d6167280849c5ce8693ad36f4c368b5af1ca
JoeXie <imxsz@qq.com>
2014-07-08 19:42:41 +0800

    修改train函数的参数，改变文件读入方式。

--- SVM_Train/SVM_Train.cpp
+++ SVM_Train/SVM_Train.cpp
@@ -12,35 +12,36 @@ using namespace cv;
 
 //函数声明
 int Train();
-void PictureDetect(Mat img);
+int train(char* positivePath, int positiveSampleCount, 
+	char* negativePath, int negativeSampleCount, char* classifierSavePath);
+void pictureDetect(Mat img, char* svmDetectorPath);
 void VideoDetect();
 
 int main(int argc, char* argv[])
 {
-	time_t startTime = time(NULL);	//记录开始时间
-
-	int trainFlag = Train();
-	
-	time_t endTime = time(NULL);	//记录结束时间
-
-	cout <<"训练所用时间：" << difftime(endTime, startTime) << "秒" << endl; //打印训练用时
-
-	if(trainFlag = 1)
-		cout<< "Train Successed!" << endl;
-	else
+	if(  0  ) //设置要不要训练
 	{
-		cout<< "Train Fail" << endl;
-		return -1;
-	};
-
-	Mat img;
-	img = imread(argv[1]);
-
-	if(argc != 2 || !img.data){
-		printf("没有图片\n");
-		return -1;
+		cout << "开始训练" << endl;
+		int trainFlag = train("E:\\SmartCity\\正样本\\正样本_完整单一人形_13\\", 865, 
+			"E:\\SmartCity\\负样本\\负样本13(自动截)\\64x128\\", 775, "E:\\SmartCity\\Result\\Result13\\");
 	}
-	PictureDetect(img);
+
+	if(  1  ) //设置要不要检测
+	{
+		Mat img;
+		//img = imread("E:\\智慧城市\\智慧城市WD\\验证数据\\1_2_01_1\\hongsilounorth_13_1920x1080_30_R1\\0005651.jpg");
+		img = imread("e:/smartcity/pic.jpg");
+		if(img.data == NULL)
+		{
+			printf("没有图片\n");
+			system("pause");
+			return -1;
+		}
+		pictureDetect(img, "E:\\SmartCity\\Result\\12472+856_RBF\\SVMDetector.txt");
+	}
+
+	system("pause");
+	return 1;
 }
 
 class Mysvm: public CvSVM
@@ -77,6 +78,217 @@ public:
 	}
 };
 
+int train(char* positivePath, int positiveSampleCount, 
+	char* negativePath, int negativeSampleCount, char* classifierSavePath)
+{
+	time_t startTime = time(NULL);	//记录开始时间
+
+	int totalSampleCount = positiveSampleCount + negativeSampleCount; //总的样本数
+	cout<<"//////////////////////////////////////////////////////////////////"<<endl;
+	cout<<"totalSampleCount: "<<totalSampleCount<<endl;
+	cout<<"positiveSampleCount: "<<positiveSampleCount<<endl;
+	cout<<"negativeSampleCount: "<<negativeSampleCount<<endl;
+
+	CvMat *sampleFeaturesMat = cvCreateMat(totalSampleCount , 3780, CV_32FC1);
+	//64*128的训练样本，该矩阵将是totalSample*3780,64*64的训练样本，该矩阵将是totalSample*1764
+	cvSetZero(sampleFeaturesMat);  
+	CvMat *sampleLabelMat = cvCreateMat(totalSampleCount, 1, CV_32FC1);//样本标识  
+	cvSetZero(sampleLabelMat);  
+
+
+	cout<<"************************************************************"<<endl;
+	cout<<"start to training positive samples."<<endl;
+
+	char* positiveNamelistPath = new char[200]; //文本文档完整文件路径
+	strcpy(positiveNamelistPath, positivePath);
+	strcat(positiveNamelistPath, "Namelist.txt"); //产生图像文件名文本文档的完整路径名
+
+	FILE* fileIn = fopen(positiveNamelistPath, "r");  //打开文件，读入，返回返回指向FILE对象的指针
+	if(fileIn == NULL)
+	{
+		printf("Can not open file."); 
+		return 0;
+	}
+
+	delete[] positiveNamelistPath; //释放内存
+
+	for(int i=0; i<positiveSampleCount; i++)  
+	{  
+		char* positiveImageName = new char[100]; //图像文件名
+		fscanf(fileIn,"%s",positiveImageName); //读取文件，遇到whitespace停止
+		char* positiveImagePath = new char[200]; //存储完整文件路径
+		strcpy(positiveImagePath, positivePath);
+		strcat(positiveImagePath,positiveImageName); //将第二个字符串拼接到第一个后面
+		printf("%s\n",positiveImagePath); //打印出完整文件路径名
+
+		cv::Mat img = cv::imread(positiveImagePath);
+		if(img.data == NULL)
+		{
+			cout<<"negative image sample load error: "<<positiveImagePath<<endl;
+			continue;
+		}
+
+		cv::HOGDescriptor hog(cv::Size(64,128), cv::Size(16,16), cv::Size(8,8), cv::Size(8,8), 9);
+		// HOGDescriptor(Size win_size=Size(64, 128), Size block_size=Size(16, 16), 
+		// Size block_stride=Size(8, 8), Size cell_size=Size(8, 8), int nbins=9, 
+		// double win_sigma=DEFAULT_WIN_SIGMA, double threshold_L2hys=0.2, 
+		// bool gamma_correction=true, int nlevels=DEFAULT_NLEVELS)
+		vector<float> featureVec; //定义一个Vector容器
+
+		hog.compute(img, featureVec, cv::Size(8,8));  //计算HOG特征
+		int featureVecSize = featureVec.size();
+
+		for (int j=0; j<featureVecSize; j++)  
+		{  		
+			CV_MAT_ELEM( *sampleFeaturesMat, float, i, j ) = featureVec[j]; //把特征复制到sampleFeaturesMat的第i行
+		
+		}  
+
+		sampleLabelMat->data.fl[i] = 1;
+
+		delete [] positiveImageName;
+		delete [] positiveImagePath;
+
+	}
+	fclose(fileIn);
+
+	cout<<"end of training for positive samples..."<<endl;
+
+	cout<<"*********************************************************"<<endl;
+	cout<<"start to train negative samples..."<<endl;
+
+	char* negativeNamelistPath = new char[200]; //文本文档完整文件路径
+	strcpy(negativeNamelistPath, negativePath);
+	strcat(negativeNamelistPath, "Namelist.txt"); //产生图像文件名文本文档的完整路径名
+	// FILE* fileIn;
+	if((fileIn=fopen(negativeNamelistPath, "r"))==NULL) //打开文件，读入，返回返回指向FILE对象的指针
+	{
+		printf("Can not open file.");
+		return 0;
+	}
+
+	delete [] negativeNamelistPath;
+	
+	for (int i=0; i<negativeSampleCount; i++)
+	{  
+		char* negativeImageName = new char[100]; //图像文件名
+		fscanf(fileIn,"%s",negativeImageName); //读取文件，遇到whitespace停止
+		char* negativeImagePath = new char[200]; //存储完整文件路径
+		strcpy(negativeImagePath, negativePath);
+		strcat(negativeImagePath,negativeImageName); //将第二个字符串拼接到第一个后面
+		printf("%s\n",negativeImagePath); //打印出完整文件路径名
+		
+		cv::Mat img = cv::imread(negativeImagePath);
+		if(img.data == NULL)
+		{
+			cout<<"negative image sample load error: "<<negativeImagePath<<endl;
+			continue;
+		}
+
+		cv::HOGDescriptor hog(cv::Size(64,128), cv::Size(16,16), cv::Size(8,8), cv::Size(8,8), 9);  
+		vector<float> featureVec; 
+
+		hog.compute(img,featureVec,cv::Size(8,8));//计算HOG特征
+		int featureVecSize = featureVec.size();  
+
+		for ( int j=0; j<featureVecSize; j ++)  
+		{  
+			CV_MAT_ELEM( *sampleFeaturesMat, float, i + positiveSampleCount, j ) = featureVec[ j ];
+		}  
+
+		sampleLabelMat->data.fl[ i + positiveSampleCount ] = -1;
+
+		delete [] negativeImageName;
+		delete [] negativeImagePath;
+	}  
+	fclose(fileIn);
+
+	cout<<"end of training for negative samples."<<endl;
+	cout<<"********************************************************"<<endl;
+	cout<<"start to train for SVM classifier."<<endl;
+
+	//开始训练SVM
+	CvSVMParams params;  
+	params.svm_type = CvSVM::C_SVC;  
+	params.kernel_type = CvSVM::RBF;  
+	params.term_crit = cvTermCriteria(CV_TERMCRIT_ITER, 1000, FLT_EPSILON);
+	params.C = 0.01;   // 默认时C＝1
+
+	Mysvm svm;
+	svm.train( sampleFeaturesMat, sampleLabelMat, 0, 0, params ); //用SVM分类器训练
+	// train(const CvMat* trainData, const CvMat* responses, const CvMat* varIdx=0, 
+	// const CvMat* sampleIdx=0, CvSVMParams params=CvSVMParams() )
+	char* trainSavePath = new char[200];
+	strcpy(trainSavePath, classifierSavePath);
+	strcat(trainSavePath, "ResultOfTrain.txt");
+	svm.save(trainSavePath);  //存储训练结果
+	cout<< trainSavePath <<"训练结果保存完毕" << endl;
+
+	delete [] trainSavePath;
+
+	cvReleaseMat(&sampleFeaturesMat);
+	cvReleaseMat(&sampleLabelMat);
+
+	int supportVectorCount = svm.get_support_vector_count();
+	cout<<"support vector size of SVM："<<supportVectorCount<<endl;
+	cout<<"************************ end of training for SVM ******************"<<endl;
+
+	CvMat *sv,*alp,*re;//所有样本特征向量 
+	sv  = cvCreateMat(supportVectorCount , 3780, CV_32FC1);
+	alp = cvCreateMat(1 , supportVectorCount, CV_32FC1);
+	re  = cvCreateMat(1 , 3780, CV_32FC1);
+	CvMat *res  = cvCreateMat(1 , 1, CV_32FC1);
+
+	cvSetZero(sv);
+	cvSetZero(re);
+  
+	for(int i=0; i<supportVectorCount; i++)
+	{
+		memcpy( (float*)(sv->data.fl+i*3780), svm.get_support_vector(i), 3780*sizeof(float));	//复制特征向量
+	}
+
+	double* alphaArr = svm.get_alpha();   //看不懂这一段,难道是透明度？
+	int alphaCount = svm.get_alpha_count();
+
+	for(int i=0; i<supportVectorCount; i++)
+	{
+        alp->data.fl[i] = alphaArr[i];
+	}
+	cvMatMul(alp, sv, re);	//re = alp*sv
+
+	int posCount = 0;
+	for (int i=0; i<3780; i++)
+	{
+		re->data.fl[i] *= -1;
+	}
+
+	//把re中的数据写到text文档里
+	char* SVMDetectorSavePath = new char[200];
+	strcpy(SVMDetectorSavePath, classifierSavePath);
+	strcat(SVMDetectorSavePath, "SVMDetector.txt");
+	FILE* fp = fopen(SVMDetectorSavePath,"wb");
+	if( NULL == fp )
+	{
+		return 1;
+	}
+	for(int i=0; i<3780; i++)
+	{
+		fprintf(fp,"%f \n",re->data.fl[i]);
+	}
+
+	float rho = svm.get_rho();
+	fprintf(fp, "%f", rho);
+	cout<< SVMDetectorSavePath <<"支持向量保存完毕"<<endl;//保存支持向量
+
+	delete [] SVMDetectorSavePath;
+	fclose(fp);
+
+	time_t endTime = time(NULL);	//记录结束时间
+	cout <<"训练所用时间：" << difftime(endTime, startTime) << "秒" << endl; //打印训练用时
+
+	return 1;
+}
+
 int Train()
 {
 	char classifierSavePath[256] = "e:/SmartCity/pedestrianDetect-peopleFlow.txt"; //存储SVM训练结果
@@ -84,8 +296,8 @@ int Train()
 	string positivePath = "E:\\SmartCity\\PositiveImages\\";  //正样本路径
 	string negativePath = "E:\\SmartCity\\NegativeImages\\";
 
-	int positiveSampleCount = 4037;  //正样本数目
-	int negativeSampleCount = 117;
+	int positiveSampleCount = 12472;  //正样本数目
+	int negativeSampleCount = 856;
 	int totalSampleCount = positiveSampleCount + negativeSampleCount;
 
 	cout<<"//////////////////////////////////////////////////////////////////"<<endl;
@@ -229,7 +441,7 @@ int Train()
 	}
 
 	//把re中的数据写到text文档里
-	FILE* fp = fopen("e:/SmartCity/hogSVMDetector-peopleFlow.txt","wb");
+	FILE* fp = fopen("e:/SmartCity/hogSVMDetector.txt","wb");
 	if( NULL == fp )
 	{
 		return 1;
@@ -247,11 +459,13 @@ int Train()
 	return 1;
 }
 
-void PictureDetect(Mat img)
+void pictureDetect(Mat img, char* svmDetectorPath)
 {	
+	time_t startTime = time(NULL);	//记录开始时间
+
 	//将文件中的数据读入到x中
 	vector<float> x;
-	ifstream fileIn("e:/SmartCity/hogSVMDetector-peopleFlow.txt", ios::in);
+	ifstream fileIn(svmDetectorPath, ios::in);
 	float val = 0.0f;
 	while(!fileIn.eof())
 	{
@@ -260,7 +474,6 @@ void PictureDetect(Mat img)
 	}
 	fileIn.close();
 
-	vector<cv::Rect>  found;
 	cv::HOGDescriptor hog(cv::Size(64,128), cv::Size(16,16), cv::Size(8,8), cv::Size(8,8), 9);
 	// HOGDescriptor(Size win_size=Size(64, 128), Size block_size=Size(16, 16), Size block_stride=Size(8, 8), 
 	// Size cell_size=Size(8, 8), int nbins=9, double win_sigma=DEFAULT_WIN_SIGMA, double threshold_L2hys=0.2, 
@@ -270,27 +483,29 @@ void PictureDetect(Mat img)
 
 	//cvNamedWindow("img", 0);
 
-	hog.detectMultiScale(img, found, 0, cv::Size(8,8), cv::Size(32,32), 1.05, 2);
+	cout << "正在检测图像！" << endl;
+	vector<cv::Rect>  found; //存储检测结果
+	hog.detectMultiScale(img, found, 0.0, cv::Size(8,8), cv::Size(32,32), 1.05, 2);
 	// detectMultiScale(const GpuMat& img, vector<Rect>& found_locations, double hit_threshold=0, 
 	// Size win_stride=Size(), Size padding=Size(), double scale0=1.05, int group_threshold=2)
-	if (found.size() > 0)
+
+	if (found.size() > 0) 
 	{
-		//for (int i=0; i<found.size(); i++)
-		//{
-		//	CvRect tempRect = cvRect(found[i].x, found[i].y, found[i].width, found[i].height);
-		//	cvRectangle(img, cvPoint(tempRect.x,tempRect.y), 
-		//		cvPoint(tempRect.x+tempRect.width,tempRect.y+tempRect.height),CV_RGB(255,0,0), 2);
-		//	//在图像上画出矩形
-		//}
+		cout << "矩形框数目："<< found.size() << endl;
 		for(int i = 0; i < found.size(); i++)
 		{
-			Rect r = found[i];
-			rectangle(img, r.tl(), r.br(), Scalar(0, 0, 255), 3);
+			rectangle(img, found[i], Scalar(0, 0, 255), 2); //在图像上画出矩形
 		}
 	}
+
+	time_t endTime = time(NULL);	//记录结束时间
+	cout <<"检测所用时间：" << difftime(endTime, startTime) << "秒" << endl; //打印训练用时
+
 	cvNamedWindow("检测行人", CV_WINDOW_NORMAL);
 	imshow("检测行人", img);
+	cout << "检测完成，已显示检测结果" << endl;
 
+//	cvSaveImage("e:/SmartCity/ProcessedImage.jpg", &img, NULL);
 	waitKey(0);
 }
 
